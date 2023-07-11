6cb49fbcaf6a850efd70c8621d14c088f89c2bfe
josiahfs <josiahfrlswt@gmail.com>
2023-07-11 10:19:33 +0700

    android internet access fix

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Suitmedia Mobile',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
